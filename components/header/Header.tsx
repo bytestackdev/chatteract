a9ec00930870f3b74beeadb22b32532c8d8f036d
Ahmad Amin <ahmad.amin@khired.com>
2024-07-23 17:09:16 +0500

    Made the navigation sticky on onscroll

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -1,19 +1,36 @@
-'use client' // Mark this component as a client component
+'use client' // Add this line to mark the component as a client component
 
-import React from 'react';
-import { FaBars } from 'react-icons/fa';
-import { Navigation } from './Navigation';
-import AuthButton from '../WebUI/AuthButton';
-import Link from 'next/link';
+import React, { useEffect, useState } from 'react'
+import { FaBars } from 'react-icons/fa'
+import { Navigation } from './Navigation'
+import AuthButton from '../WebUI/AuthButton'
+import Link from 'next/link'
 
 interface HeaderProps {
-  isLoggedIn: boolean;
+  isLoggedIn: boolean
 }
 
 const Header: React.FC<HeaderProps> = ({ isLoggedIn }) => {
+  const [hasShadow, setHasShadow] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setHasShadow(window.scrollY > 0);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    // Cleanup event listener on component unmount
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <div className="sticky top-0 w-full bg-white z-50 transition-shadow duration-300 shadow-none">
-      <div className="flex flex-row justify-between items-center p-4 md:p-10">
+    <div
+      className={`sticky top-0 w-full bg-white transition-shadow duration-300 ${hasShadow ? 'shadow-lg' : ''} z-50`}
+    >
+      <div className="flex flex-row justify-between items-center p-4">
         <div className="flex flex-row gap-2 items-center">
           <div className="block md:hidden">
             <FaBars />
@@ -31,7 +48,7 @@ const Header: React.FC<HeaderProps> = ({ isLoggedIn }) => {
         <Navigation />
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Header;
+export default Header
