2c6549f7a529a0f953d7770da2241280cef6d1a3
Ahmad Amin <ahmad.amin@khired.com>
2024-07-29 13:03:59 +0500

    added the custom skeleton on the settings page plus fixed the settings page layout issue

--- components/dashboard/setting/SettingMainView.tsx
+++ components/dashboard/setting/SettingMainView.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import SettingsLayout from '@/app/layouts/SettingsLayout'
+import CustomSkeleton from '@/components/ui/CustomSkeleton'
 import { useRouter } from 'next/navigation'
 import React, { useEffect } from 'react'
 
@@ -11,14 +12,9 @@ const SettingMainView = () => {
 		router.push('/dashboard/settings/general')
 	}, [router])
 	
-	return (
-		<SettingsLayout>
-			<div className=' flex justify-center items-center h-full mt-20'>
-				settings Page
-			</div>
-		</SettingsLayout>
-
-	)
+	return <>
+		<CustomSkeleton />
+	</>
 }
 
 export default SettingMainView
